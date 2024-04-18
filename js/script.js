d7252548aa98a3b2f8175700b1d20849177a95fd
GeneratorBot <generatorbot@criodo.com>
2024-04-18 16:53:39 +0530

    Updating repository

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// Sample data
+const questions = [
+    {
+    text: "Which language is primarily used for web app development?",
+    options: ["C#", "Python", "JavaScript", "Swift"],
+    correct: 2
+},
+{
+    text: "Which of the following is a relational database management system?",
+    options: ["Oracle", "Scala", "Perl", "Java"],
+    correct: 0
+},
+{
+    text: "In which language is memory management provided by JVM?",
+    options: ["Java", "C", "C++", "Python"],
+    correct: 0
+},
+{
+    text: "What does HTML stand for?",
+    options: ["Hyperlink and Text Markup Language", "High Technology Modern Language", "Hyper Text Markup Language", "Home Tool Markup Language"],
+    correct: 2
+},
+{
+    text: "Which of the following is not a valid variable name in Python?",
+    options: ["_myVar", "myVar2", "2myVar", "my_var"],
+    correct: 2
+},
+{
+    text: "Which of the following is not an object-oriented programming language?",
+    options: ["Java", "C#", "Scala", "C"],
+    correct: 3
+},
+{
+    text: "Which tool is used to ensure code quality in JavaScript?",
+    options: ["JSLint", "TypeScript", "Babel", "Webpack"],
+    correct: 0
+},
+{
+    text: "In which data structure, elements are added at one end and removed from the other?",
+    options: ["Array", "Stack", "Queue", "LinkedList"],
+    correct: 2
+},
+{
+    text: "What is the primary use of the Git command 'clone'?",
+    options: ["To stage changes", "To copy a repository", "To switch to a different branch", "To list all the files in a repository"],
+    correct: 1
+},
+{
+    text: "What does API stand for in the context of programming?",
+    options: ["Apple Pie Interface", "Application Programming Interface", "Advanced Peripheral Integration", "Application Process Integration"],
+    correct: 1
+}
+];
+
+
+submitButton.addEventListener("click", () => {
+});
+
+nextButton.addEventListener("click", () => {
+});
